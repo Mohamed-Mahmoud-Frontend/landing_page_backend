c3b03f259fa489cba598b90c50193959fa2ce4fd
Mohamed Mahmoud <mohamedmkamal19@gmail.com>
2024-09-19 17:46:07 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+let orders = [];
+
+app.post('/api/orders', (req, res) => {
+  const order = req.body;
+  orders.push(order);
+  res.status(201).send({ message: 'Order received', order });
+});
+
+app.get('/api/orders', (req, res) => {
+  res.status(200).json(orders);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
