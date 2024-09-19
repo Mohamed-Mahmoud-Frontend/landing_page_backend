ae0016fb1147f02367c8ee0ef19fa6793270ce8d
Mohamed Mahmoud <mohamedmkamal19@gmail.com>
2024-09-19 18:23:56 +0300

    edit file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 5000;
+const PORT = 5000 || process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
